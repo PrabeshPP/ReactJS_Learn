d4814d4a3ad897ad0f5930fca764a0a2da074fda
Prabesh <prabesh.bista369@gmail.com>
2022-01-20 21:17:58 +0545

    .

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <>
     <h1>My name is prabesh Bista</h1>
+    <h2>Hello every one .It's me Prabesh Bista</h2>
+    <p>Hi,currently I am learning about reactjs ,but i am a mid-level app developer. </p>
     </>
   );
 }
