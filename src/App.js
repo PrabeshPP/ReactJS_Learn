449539ad7e02cd9342503585bd7255c58aed643d
Prabesh <prabesh.bista369@gmail.com>
2022-01-22 21:30:40 +0545

    components,props

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React  from 'react';
 import ExpenseItem from './components/ExpenseItem';
 
+
 function App() {
   const expenses=[
     {
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,6 @@
 // import React from "react";
 import './ExpenseItem.css'
-
+import ExpenseDate from './components/ExpenseDate';
 
 function ExpenseItem(props){
     
