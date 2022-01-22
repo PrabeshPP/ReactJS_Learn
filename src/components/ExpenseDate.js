74b41cff71bcf193620d4c3062f2d63ef87ad6cd
Prabesh <prabesh.bista369@gmail.com>
2022-01-22 21:47:07 +0545

    splitted component from each other

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,12 +1,15 @@
+import './ExpenseDate.css';
+
 function ExpenseDate(props){
     const month=props.date.toLocaleString('en-US',{month:'long'});
     const day=props.date.toLocaleString('en-US',{day:'2-digit'})
     const year=props.date.getFullYear();
 return(
  <div className="ExpenseItem-date">
-<div>{month}</div>
-<div>{day}</div>
-<div>{year}</div>
+<div className='ExpenseItem-date__month'>{month}</div>
+<div className='ExpenseItem-date__year'>{year}</div>
+<div className='ExpenseItem-date__day'>{day}</div>
+
 </div>
 );
 
