e15504d864f0e4ea24bf525a74a819fd60c96b5e
Prabesh <prabesh.bista369@gmail.com>
2022-01-26 18:29:38 +0545

    form

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,15 @@
+const NewExpense=()=>{
+    return (
+        <div>
+            <form>
+                <input>Date</input>
+                <input>Title</input>
+                <input>Amount</input>
+            </form>
+        </div>
+    )
+
+}
+
+
+export default NewExpense;
