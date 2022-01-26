4c7a332b5676db0b331e30c9c350f71a7a0bd96d
Prabesh <prabesh.bista369@gmail.com>
2022-01-26 16:25:50 +0545

    .

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,23 +4,28 @@ import Card from '../UI/Card';
 import React from 'react';
 
 function ExpenseItem(props){
+    let title=props.title;
+   let Alert=()=>{
+       
+        title="Prabesh Bista";
+        
+    }
+    
     return (
         <Card className="ExpenseItem-main">
         <ExpenseDate date={props.date}/>    
        <div className="ExpenseItem-description">
-    <h2>{props.title}</h2>
+    <h2>{title}</h2>
        <div className="ExpenseItem-amount">{"$"+props.amount}</div>
       
        </div>
-       <button onClick={function Alert(){
-           let square=10*10;
-           alert(square);
-       }}>Change Title</button>
+       <button onClick={Alert}>Change Title</button>
         </Card>
     
     );
 
 }
+
 
 
 export default ExpenseItem;
