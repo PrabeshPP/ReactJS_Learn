994d78dae4ed9733bddaf1289753ac85219c060c
Prabesh <prabesh.bista369@gmail.com>
2022-01-27 10:07:12 +0545

    useState

--- src/components/NewExpense/Form.js
+++ src/components/NewExpense/Form.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import './Form.css';
+
 const Form=()=>{
+    const [enteredTitle,setTitle]=useState("");
+    const title_change=(event)=>{
+        setTitle(event.target.value);
+    
+
+    }
+
+    const [enteredAmount,setAmount]=useState(0);
+
+    const amount_change=(event)=>{
+        setAmount(event.target.value);
+        
+    }
+
+    const [enteredDate,setDate]=useState("");
+    const date_change=(event)=>{
+        setDate(event.target.value);
+        
+    }
     return (
        <form>
             <div className="Form">
                 
                 <div className="Form-title">
                     <label>Title</label>
-                    <input type={"text"}/>
+                    <input type={"text"} onChange={title_change} />
                 </div>
                 <div className="Form-amount">
                     <label>Amount</label>
-                    <input type={"number"} min={"0.01"} step={"0.01"}/>
+                    <input type={"number"} min={"0.01"} step={"0.01"} onChange={amount_change} />
                 </div>
                 <div className="Form-date">
                     <label>Date</label>
-                    <input type={"date"} min={"2019-01-01"} max={"2022-26-1"}/>
+                    <input type={"date"} min={"2019-01-01"} max={"2022-26-1"} onChange={date_change} />
                 </div>
                 <div className="Form-Button">
                     <button type="submit">Add Expense</button>
