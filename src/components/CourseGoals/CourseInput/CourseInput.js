bc6cf8288e441820cd42e592d9b9850f55cfab29
Prabesh <prabesh.bista369@gmail.com>
2022-02-15 13:41:15 +0545

    .

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -25,11 +25,12 @@ const CourseInput = props => {
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label style={{color:isValid?'black':'red'}}>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler}  style={{color:isValid?'black':'red'}}/>
-      </div>
-      {
-       <p style={{color:'red', size:'1px'}}>{isValid?"":"Empty String"}</p>
+        <input type="text" style={{border:isValid?"1px solid #ccc":"1px solid red"}}  onChange={goalInputChangeHandler}  />
+        {
+       <p style={{color:'red', size:'1px'}}>{isValid?"":"Invalid String"}</p>
       }
+      </div>
+     
       
       <Button type="submit">Add Goal</Button>
     </form>
