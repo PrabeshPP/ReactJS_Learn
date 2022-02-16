66d6c8205da8c9a709fed8bf1609ec4cf2fa3ee0
Prabesh <prabesh.bista369@gmail.com>
2022-02-16 16:23:43 +0545

    Css style Module

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from'./Button.css';
+import styles from'./Button.module.css';
 
 
 // //Styled Components
@@ -35,7 +35,7 @@ import styles from'./Button.css';
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
