973fcae0f5d3135541215618f1bb94a59dabc2b1
Prabesh <prabesh.bista369@gmail.com>
2022-02-16 16:06:26 +0545

    Styled Components

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,11 @@
-import React from 'react';
 import styled from 'styled-components';
 
 import './Button.css';
 
-const Button=styled.button`
 
+//Styled Components
+const Button=styled.button`
+  width:100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -12,7 +13,11 @@ const Button=styled.button`
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
-  border-radius:25px;
+  
+
+  @media (min-width:768px){
+    width:auto;
+  }
 
 &:focus {
   outline: none;
@@ -26,6 +31,8 @@ const Button=styled.button`
 }
 
 `;
+
+//!Styled Components
 
 // const Button = props => {
 //   return (
