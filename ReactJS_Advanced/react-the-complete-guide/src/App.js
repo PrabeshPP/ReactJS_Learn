ce384fb9a5d1ff717bd76af85a76b55acc55eea9
Prabesh <prabesh.bista369@gmail.com>
2022-03-13 09:51:13 +0545

    Virtual DOM

--- ReactJS_Advanced/react-the-complete-guide/src/App.js
+++ ReactJS_Advanced/react-the-complete-guide/src/App.js
@@ -1,11 +1,18 @@
 import React,{useState} from "react";
 import './App.css';
+import Button from "./components/UI/Button";
 
 function App() {
   const[showParagraph,setShowParagraph]=useState(false);
+
+  const toggleParagraphHandler=()=>{
+    setShowParagraph(true);
+  }
   return (
     <div className="app">
-      <h1>Hi there</h1>
+    <h1>Hi There</h1>
+      {showParagraph && <p>This is New</p>}
+      <Button onClick={toggleParagraphHandler}>ToggleParagraphHandler</Button>
     </div>
   );
 }
