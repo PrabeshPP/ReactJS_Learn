d3253dd893e51f15b519f8c63bb08a104ed9b706
Prabesh <prabesh.bista369@gmail.com>
2022-03-14 10:22:59 +0545

    added try,catch block

--- ReactJS_Advanced/backend-database/src/App.js
+++ ReactJS_Advanced/backend-database/src/App.js
@@ -7,23 +7,43 @@ function App() {
 
   const[movies,setMovies]=useState([]);
   const [isLoading,setISLoading]=useState(false);
+  const [error,setError]=useState(null);
 
   async function fetchMoviesHandler(){
     setISLoading(true);
-    const response=await fetch('https://swapi.dev/api/films');
-    const data=await response.json();
-    const transformedMovies=data.results.map((movieData)=>{
-        return {
-          id:movieData.episode_id,
-          title:movieData.title,
-          openingText:movieData.opening_crawl,
-          releaseDate:movieData.relase_date
-
+    setError(null);
+    try{
+        const response=await fetch('https://swapi.dev/api/films/');
+        
+        if(response.status!=200){
+          throw new Error("Unable to load the data")
+         
         }
-      });
+        const data=await response.json();
+        const transformedMovies=data.results.map((movieData)=>{
+          return {
+            id:movieData.episode_id,
+            title:movieData.title,
+            openingText:movieData.opening_crawl,
+            releaseDate:movieData.relase_date
+  
+          }
+        });
+        setMovies(transformedMovies);
+       
 
-     setMovies(transformedMovies);
-     setISLoading(false);
+    
+      
+    }catch (err) {
+      setError(err.message)
+
+    }
+    setISLoading(false);
+
+
+  
+
+     
 
    
   };
@@ -55,8 +75,9 @@ function App() {
       </section>
       <section>
         {!isLoading && movies.length>0  && <MoviesList movies={movies} />}
-        {!isLoading && movies.length==0 && <p>Not Found</p>}
+        {!isLoading && movies.length==0 && !error &&<p>Not Found</p>}
         {isLoading && <p>Loading...</p>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
