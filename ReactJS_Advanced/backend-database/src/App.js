cde9044d1dc4a928d30497dbe6a3eb84a95ce10b
Prabesh <prabesh.bista369@gmail.com>
2022-03-14 09:58:17 +0545

    loading state added

--- ReactJS_Advanced/backend-database/src/App.js
+++ ReactJS_Advanced/backend-database/src/App.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
+
+  const[movies,setMovies]=useState([]);
+  const [isLoading,setISLoading]=useState(false);
+
+  async function fetchMoviesHandler(){
+    setISLoading(true);
+    const response=await fetch('https://swapi.dev/api/films');
+    const data=await response.json();
+    const transformedMovies=data.results.map((movieData)=>{
+        return {
+          id:movieData.episode_id,
+          title:movieData.title,
+          openingText:movieData.opening_crawl,
+          releaseDate:movieData.relase_date
+
+        }
+      });
+
+     setMovies(transformedMovies);
+     setISLoading(false);
+
+   
+  };
+
+  
+
+
+
+
   const dummyMovies = [
     {
       id: 1,
@@ -22,10 +51,11 @@ function App() {
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
