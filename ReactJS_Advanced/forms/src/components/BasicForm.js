b2a7bb2a70b061d760999f818e4f6855075cfae3
Prabesh <prabesh.bista369@gmail.com>
2022-03-25 09:31:48 +0545

    Challange Completed

--- ReactJS_Advanced/forms/src/components/BasicForm.js
+++ ReactJS_Advanced/forms/src/components/BasicForm.js
@@ -1,22 +1,67 @@
+import useBasicForm from "../hooks/use-inputform";
+
 const BasicForm = (props) => {
+
+  const{enteredValueIsValid:firstNameValid,
+  hasError:hasError,
+valueInputChangeHandler:nameInputChangeHandler,
+valueInputBlurHandler:nameInputBlurHandler,
+reset:resetFirstName}
+  =useBasicForm(value=>value.trim()!="");
+
+
+  const{enteredValueIsValid:lastNameValid,
+    hasError:hasErrorInLastName,
+  valueInputChangeHandler:lastnameInputChangeHandler,
+  valueInputBlurHandler:lastnameInputBlurHandler,
+  reset:resetLastName}
+    =useBasicForm(value=>value.trim()!="");
+
+    const{enteredValueIsValid:emaiIsValid,
+      hasError:hasErrorInEmail,
+    valueInputChangeHandler:emailInputChangeHandler,
+    valueInputBlurHandler:emailInputBlurHandler,
+    reset:resetemail}
+      =useBasicForm(value=>value.includes("@"));
+
+  let formisValid=false;
+  if(firstNameValid && lastNameValid && emaiIsValid){
+    formisValid=true;
+  }
+
+  const onSubmitHandler=(event)=>{
+    event.preventDefault();
+    resetFirstName();
+    resetLastName();
+    resetemail();
+
+  }
+
+  const firstNameClasses=hasError?"form-control invalid":"form-control"; 
+  const lastNameClasses=hasErrorInLastName?"form-control invalid":"form-control"; 
+  const emailClasses=hasErrorInEmail?"form-control invalid":"form-control"; 
+
   return (
-    <form>
+    <form onSubmit={onSubmitHandler}>
       <div className='control-group'>
-        <div className='form-control'>
+        <div className={firstNameClasses}>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input type='text' id='name' onBlur={nameInputBlurHandler} onChange={nameInputChangeHandler} />
+          {hasError && <p className="error-text">name cannot be empty</p>}
         </div>
-        <div className='form-control'>
+        <div className={lastNameClasses}>
           <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+          <input type='text' id='name' onBlur={lastnameInputBlurHandler} onChange={lastnameInputChangeHandler}/>
+          {hasErrorInLastName && <p className="error-text">name cannot be empty</p>}
         </div>
       </div>
-      <div className='form-control'>
+      <div className={emailClasses}>
         <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name'/>
+        <input type='text' id='name' onBlur={emailInputBlurHandler} onChange={emailInputChangeHandler}/>
+        {hasErrorInEmail && <p className="error-text">email should contain "@"</p>}
       </div>
       <div className='form-actions'>
-        <button>Submit</button>
+        <button disabled={!formisValid}>Submit</button>
       </div>
     </form>
   );
--- ReactJS_Advanced/forms/src/hooks/use-inputform.js
+++ ReactJS_Advanced/forms/src/hooks/use-inputform.js
@@ -1,16 +1,38 @@
-const userBasicForm=()=>{
+import { useState } from "react";
+
+const useBasicForm=(isValid)=>{
+    const [enteredValue,setEnteredValue]=useState("");
+    const [isTouched,setIsTouched]=useState(false);
+
+    const enteredValueIsValid=isValid(enteredValue);
+    const hasError=!enteredValueIsValid && isTouched;
 
     const valueInputBlurHandler=(event)=>{
+        setIsTouched(true);
 
 
     }
 
     const valueInputChangeHandler=(event)=>{
-        
+        setEnteredValue(event.target.value);
+
+    }
+
+    const reset=()=>{
+        setIsTouched(false);
+        setEnteredValue("");
+    }
+
+    return{
+        enteredValueIsValid,
+        hasError,
+        valueInputChangeHandler,
+        valueInputBlurHandler,
+        reset
     }
 
 
 
 }
 
-export default userBasicForm;
+export default useBasicForm;
