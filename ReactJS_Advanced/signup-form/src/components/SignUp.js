fbf6922d6899ba8e271372e570cd448e36f5afc7
Prabesh <prabesh.bista369@gmail.com>
2022-03-26 09:49:54 +0545

    Basic SignUp Form Designed

--- ReactJS_Advanced/signup-form/src/components/SignUp.js
+++ ReactJS_Advanced/signup-form/src/components/SignUp.js
@@ -3,14 +3,69 @@ import classes from "./SignUp.module.css";
 
 const SignUp=()=>{
 
-   return <form>
-       <div className={classes.box1}>
-           <div className={classes.firstName}>
+
+    const onFormSubmitHandler=(event)=>{
+        event.preventDefault();
+    }
+
+   return <form onSubmit={onFormSubmitHandler}>
+        {/* Name Field */}
+        <div className={classes.box1}>
+       
+
+       <div className={classes.container}>
+       <div className={classes.box2}>
+           <div>
                <label className={classes.label}>First Name:</label>
+               <input className={classes.input} type="text" />
+           </div>
+           <div>
+               <label className={classes.label}>Last Name:</label>
                <input className={classes.input} type="text" />
            </div>
            
        </div>
+       {/* Email Field  */}
+       <div className={classes.box3}>
+       <div>
+           <label className={classes["label-email"]}>Email:</label>
+           <input className={classes["input-email"]} type="text" />
+        </div>
+        <div>
+           <label className={classes["label-email"]}>Phone Number:</label>
+           <input className={classes["input-email"]} type="number" />
+        </div>
+
+       </div>
+       <div className={classes.box4}>
+           <label className={classes["radio-label"]}>Gender:</label>
+           <div className={classes["radio-input"]}>
+           <div className={classes["radio-input-1"]}>
+           <input value="Male" type="radio" name="gender"/>
+           <span className={classes.span}>Male</span>
+           </div>
+           <div className={classes["radio-input-1"]}>
+           <input type="radio" value="Female" name="gender" />
+           <span className={classes.span}>Female</span>
+           </div>
+           </div>
+       </div>
+
+
+
+       </div>
+         <div className={classes.signUP}>
+             <button>SignUp</button>
+         </div>
+
+        </div>
+
+        
+       
+
+       
+
+
    </form>
 
 }
