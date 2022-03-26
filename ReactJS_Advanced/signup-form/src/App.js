fda3243a5fa1f92e392d12358d0adf4b3e59aba7
Prabesh <prabesh.bista369@gmail.com>
2022-03-26 14:53:36 +0545

    Card Added

--- ReactJS_Advanced/signup-form/src/App.js
+++ ReactJS_Advanced/signup-form/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import SignUp from './components/SignUp';
+import LeftComponent from './components/SignUp_Left';
 function App() {
   return (
     <div className="App">
+    <LeftComponent/>
     <SignUp/>
 
     </div>
--- ReactJS_Advanced/signup-form/src/components/SignUp_Left.js
+++ ReactJS_Advanced/signup-form/src/components/SignUp_Left.js
@@ -1,7 +1,23 @@
+import classes from "./SignUp_Left.module.css";
+
+import img from "C:/Users/prabe/OneDrive/Desktop/ReactJS/reactJS_intro/ReactJS_Advanced/signup-form/src/assets/chip.png"
+
 const LeftComponent=()=>{
     return(
-        <div >
-
+        <div className={classes.card}>
+        <div className={classes.h1}>
+        <h1>My Coin</h1>
+        </div>
+        <div>
+        <img className={classes.chip} src={img} />
+        </div>
+        <h2 className={classes["card-number"]}>XXXX XXXX XXXX XXXX</h2>
+        <div className={classes["card-date"]}>
+            <h3>2090</h3>
+            <h3>10/12</h3>
+            <h3>9/25</h3>
+        </div>
+        <h4 className={classes.h4}>Lord Voldemort</h4>
         </div>
     )
 
