be0e48173ef4e95aab9d1fd676a6cf2d542bd815
Prabesh <prabesh.bista369@gmail.com>
2022-04-05 16:55:49 +0545

    payload in redux

--- Redux/redux_basic/src/components/Counter.js
+++ Redux/redux_basic/src/components/Counter.js
@@ -4,8 +4,12 @@ import { useDispatch, useSelector } from "react-redux";
 const Counter = () => {
   const dispatch = useDispatch();
   const incrementHandler = () => {
-    dispatch({ type: "INCREMENT" });
+    dispatch({ type: "INCREMENT",value:1 });
   };
+
+  const incrementHandler1=()=>{
+    dispatch({type:"INCREMENT",value:5});
+  }
 
   const decrementHandler = () => {
     dispatch({ type: "DECREMENT" });
@@ -20,6 +24,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={incrementHandler1}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
