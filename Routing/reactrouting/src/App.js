b24c13a0f429080acef2c63e7fb3153f90b02276
Prabesh <prabesh.bista369@gmail.com>
2022-04-15 10:05:31 +0545

    Active Link in React Routing

--- Routing/reactrouting/src/App.js
+++ Routing/reactrouting/src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Welcome from "./Pages/Welcome";
 import Products from "./Pages/Products";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <div className="App">
+    <MainHeader/>
       <Routes>
       <Route path="/w" element={<Welcome/>}/>
       <Route path="/p" element={<Products/>}/>  
