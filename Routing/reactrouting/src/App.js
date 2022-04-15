0c4b2f3be74b40c3a8377774be39650bd169ea33
Prabesh <prabesh.bista369@gmail.com>
2022-04-15 12:59:04 +0545

    Dynamic Routing

--- Routing/reactrouting/src/App.js
+++ Routing/reactrouting/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Welcome from "./Pages/Welcome";
 import Products from "./Pages/Products";
 import MainHeader from "./components/MainHeader";
+import ProductDetail from "./Pages/ProductDetail";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
       <Route path="/w" element={<Welcome/>}/>
       <Route path="/p" element={<Products/>}/>  
+      <Route path="/product-detail/:productId" element={<ProductDetail/>}/>  
       </Routes>
     </div>
   );
--- Routing/reactrouting/src/Pages/Products.js
+++ Routing/reactrouting/src/Pages/Products.js
@@ -1,5 +1,14 @@
+import React from "react";
+
 const Products = () => {
-    return <h1>The Products Page.</h1>;
+    return <React.Fragment>
+      <h1>The Products Page.</h1>
+      <ul>
+        <li>A Book</li>
+        <li>A Mobile</li>
+        <li>A Computer</li>
+      </ul>
+    </React.Fragment>;
   };
   
   export default Products;
