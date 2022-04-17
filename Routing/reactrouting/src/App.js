de571edb051583a998e633cd4bcbdf424aa93538
Prabesh <prabesh.bista369@gmail.com>
2022-04-17 08:36:08 +0545

    practicing react routing

--- Routing/reactrouting/src/App.js
+++ Routing/reactrouting/src/App.js
@@ -1,8 +1,12 @@
+import {Route,Routes} from "react-router-dom";
+
 function App() {
   return (
-    <div>
-
-    </div>
+   <Routes>
+     <Route  path="/quotes"/>
+     <Route path="/quotes/:quoteId"/>
+     <Route path="/new-quote"/>
+   </Routes>
   );
 }
 
