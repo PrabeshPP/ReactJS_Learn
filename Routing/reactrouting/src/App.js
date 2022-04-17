ffe5899dfa77bc5757de4dd440aa507ce9292843
Prabesh <prabesh.bista369@gmail.com>
2022-04-17 10:00:05 +0545

    layout Wrapper

--- Routing/reactrouting/src/App.js
+++ Routing/reactrouting/src/App.js
@@ -2,13 +2,16 @@ import {Route,Routes,Navigate} from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
 import DetailQuote from "./pages/DetailQuote";
 import NewQuote from "./pages/NewQuote";
+import Comments from "./components/comments/Comments";
 
 function App() {
   return (
    <Routes>
-   <Route path="/" element={<Navigate to="/quotes" />}/>
+   <Route path="/:any" element={<Navigate to="/quotes" />}/>
      <Route  path="/quotes" element={<AllQuotes/>} />
-     <Route path="/quotes/:quoteId" element={<DetailQuote/>} />
+     <Route path="/quotes/:quoteId/*" element={<DetailQuote/>} >
+      <Route path="comments" element={<Comments/>} />
+     </Route>
      <Route path="/new-quote" element={<NewQuote/>} />
    </Routes>
   );
--- Routing/reactrouting/src/pages/DetailQuote.js
+++ Routing/reactrouting/src/pages/DetailQuote.js
@@ -1,18 +1,16 @@
 import React from "react";
-import { Routes, useParams } from "react-router-dom";
-import { Route } from "react-router-dom";
-import Comments from "../components/comments/Comments";
+import {useParams,Outlet, Link } from "react-router-dom";
+
 
 const DetailQuote=()=>{
     const params=useParams();
     return(
         <React.Fragment>
             <h1>Detail Quote page</h1>
-            <p>{params.quoteId}</p>
-            <Routes>
-            <Route path={`/quote/${params.quoteId}/comments`} element={<Comments/>}/>
-            </Routes>
-
+            <p><Link to={`/quotes/${params.quoteId}/comments`} >{params.quoteId}</Link></p>
+           
+            
+            <Outlet/>
         </React.Fragment>
     )
 
