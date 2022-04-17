fd2886526b37e9e14b87de042e34be7e0a789337
Prabesh <prabesh.bista369@gmail.com>
2022-04-17 10:00:08 +0545

    layout Wrapper

--- Routing/reactrouting/src/App.js
+++ Routing/reactrouting/src/App.js
@@ -3,17 +3,22 @@ import AllQuotes from "./pages/AllQuotes";
 import DetailQuote from "./pages/DetailQuote";
 import NewQuote from "./pages/NewQuote";
 import Comments from "./components/comments/Comments";
+import MainNavigation from "./components/layout/MainNavigation";
+import React from "react";
 
 function App() {
   return (
-   <Routes>
-   <Route path="/:any" element={<Navigate to="/quotes" />}/>
+   <React.Fragment>
+   <MainNavigation/>
+     <Routes>
+   <Route path="/" element={<Navigate to="/quotes" />}/>
      <Route  path="/quotes" element={<AllQuotes/>} />
      <Route path="/quotes/:quoteId/*" element={<DetailQuote/>} >
       <Route path="comments" element={<Comments/>} />
      </Route>
      <Route path="/new-quote" element={<NewQuote/>} />
    </Routes>
+   </React.Fragment>
   );
 }
 
