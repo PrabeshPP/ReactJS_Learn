03b5021f4fc37fd113e02601651365568acfbd87
Prabesh <prabesh.bista369@gmail.com>
2022-04-18 13:58:44 +0545

    impl. DetailQuote

--- Routing/reactrouting/src/pages/DetailQuote.js
+++ Routing/reactrouting/src/pages/DetailQuote.js
@@ -1,13 +1,20 @@
 import React from "react";
 import {useParams,Outlet, Link } from "react-router-dom";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
+const Dummy_Data = [
+    { id: "q1", author: "Prabesh", text: "Winner never quit!!" },
+    { id: "q2", author: "Prabesh the great", text: "Learner are the great leader!!" },
+  ];
 
 const DetailQuote=()=>{
+
     const params=useParams();
+    const quote=Dummy_Data.find((item)=>item.id===params.quoteId);
+   
     return(
         <React.Fragment>
-            <h1>Detail Quote page</h1>
-            <p><Link to={`/quotes/${params.quoteId}/comments`} >{params.quoteId}</Link></p>
+            <HighlightedQuote text={quote.text} author={quote.author}/>
            
             
             <Outlet/>
